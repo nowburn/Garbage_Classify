4f460d4a1babd32a9c7ff0a0c07d53eecd262edb
nowburn <newcoding@163.com>
2019-08-17 22:31:08 +0800

    update: can run in local

---
+++ src/run_train.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+python run.py \
+      --data_url='../datasets/garbage_classify/train_data' \
+      --train_url='../model_snapshots' \
+      --num_classes=40 \
+      --deploy_script_path='./deploy_scripts' \
+      --max_epochs=20 \
+      #--test_data_url='../datasets/test_data'
